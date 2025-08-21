1ddc5a6d02a968919c302ca7e0ee3b993265b06c
Gowtham-beep <gn86923@gmail.com>
2025-08-21 16:54:48 +0530

    Feat:create the fabricator repository class with  CRUD function

--- src/modules/fabricator/dtos/fabricator.dtos.ts
+++ src/modules/fabricator/dtos/fabricator.dtos.ts
@@ -1,7 +1,6 @@
 import z from "zod";
 
 export const CreateFabricatorSchema=z.object({
-    createdById:z.string(),
     fabName:z.string().min(1,{message:"Fabricator name is required"}),
     website:z.url({message:"Invalid website URL"}).nullable().optional(),
     drive:z.url({message:"Invalid drive link"}).nullable().optional(),
---
+++ src/modules/fabricator/repositories/repository.ts
@@ -0,0 +1,53 @@
+import { 
+     CreateFabricatorInput,
+     UpdateFabricatorInput, 
+     GetFabricatorInput, 
+     DeleteFabricatorInput
+ } from "../dtos";
+
+ import prisma from "../../../config/database/client";
+
+ export class FabricatorRepository {
+      async create(data: CreateFabricatorInput, userId: string) {
+    return prisma.fabricator.create({
+      data: {
+        ...data,
+        createdById: userId,
+      },
+    });
+  }
+
+  // Get all fabricators
+  async findAll() {
+    return prisma.fabricator.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+  }
+
+  // Get fabricator by ID
+  async findById(input: GetFabricatorInput){
+    return prisma.fabricator.findUnique({
+      where: { id: input.id },
+    });
+  }
+
+  // Update fabricator
+  async update(input: GetFabricatorInput, data: UpdateFabricatorInput){
+    return prisma.fabricator.update({
+      where: { id: input.id },
+      data: {
+        fabName: data.fabName,
+        website: data.website ?? null,
+        drive: data.drive ?? null,
+        files: data.files ?? [],
+      },
+    });
+  }
+
+  // Delete fabricator
+  async delete(input: DeleteFabricatorInput){
+    return prisma.fabricator.delete({
+      where: { id: input.id },
+    });
+  }
+ }
