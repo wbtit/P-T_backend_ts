6e8dad1a7de596c3c25e2ab3c14ab65acef7a3fb
Gowtham-beep <gn86923@gmail.com>
2025-08-21 16:33:22 +0530

    Feat:added upload file utils to config the file details

---
+++ src/modules/uploads/fileUtil.ts
@@ -0,0 +1,12 @@
+import path from "path";
+
+export function mapUploadedFiles(files: Express.Multer.File[], entity: string) {
+  if (!files || files.length === 0) return [];
+
+  return files.map((file) => ({
+    filename: file.filename,
+    originalName: file.originalname,
+    id: file.filename.split(".")[0], // Extract UUID
+    path: path.join(`/public/${entity}/${file.filename}`), // Entity-specific relative path
+  }));
+}
