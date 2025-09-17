1a004af2ddc072954ffc7c3c03ebcc875c051a28
Gowtham-beep <gn86923@gmail.com>
2025-09-17 11:57:11 +0530

    Feat: add optional mileStone_id field to createTaskDto for task creation

--- src/modules/tasks/dtos/task.dto.ts
+++ src/modules/tasks/dtos/task.dto.ts
@@ -5,6 +5,7 @@ import { Prisma } from "@prisma/client";
 export const createTaskDto = z.object({
     name: z.string().min(1, "Task name is required"),
     description: z.string().min(1, "Task description is required"),
+    mileStone_id:z.string().optional(),
     status: z.enum(TaskStatus),
     priority: z.number().min(1).max(5),
     due_date: z.preprocess((val) => val ? new Date(val as string) : undefined, z.date()),
