4140fc4b1f8c9e74d824dfa3dfe44847bc12cfdd
Gowtham-beep <gn86923@gmail.com>
2025-10-07 16:03:46 +0530

    Feat: add CoResponse module with controller, DTOs, and repository structure

---
+++ src/modules/CO/dtos/coResponse.dto.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+import { COSTATUS, Prisma } from "@prisma/client";
+
+export const CoResponseSchema = z.object({
+  Status: z.enum(COSTATUS),
+  CoId: z.string().optional(),
+  description: z.string().min(2).max(500),
+  userId: z.string(),
+  parentResponseId: z.string().optional(),       
+    files: z
+              .union([
+                z.array(z.any()),
+                z.literal(null),
+              ])
+              .transform((val) => (val === null ? Prisma.JsonNull : val))
+              .optional(),
+});
+export const UpdateCoResponseSchema = CoResponseSchema.partial();
+
+export type CreateCoResponseDto = z.infer<typeof CoResponseSchema>;
+export type UpdateCoResponseDto = z.infer<typeof UpdateCoResponseSchema>;
+
