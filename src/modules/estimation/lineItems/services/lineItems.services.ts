38673dc889b1554c21aa85f4507361e79f24b815
Gowtham-beep <gn86923@gmail.com>
2025-12-12 12:05:15 +0530

    feat(line-items): add LineItemsService with CRUD operations for line items and groups

---
+++ src/modules/estimation/lineItems/services/lineItems.services.ts
@@ -0,0 +1,45 @@
+import {lineItemDto,
+        updateLineItemDto,
+        lineItemGroupDto,
+        updateLineItemGroupDto
+} from "../dtos";
+import {LineItemsRepository} from "../repositories"
+
+export class LineItemsService{
+    private lineItemsRepo:LineItemsRepository;
+    constructor(){
+        this.lineItemsRepo=new LineItemsRepository();
+    }
+//Line Item Groups
+    async createLineItemGroup(data:lineItemGroupDto){
+        return await this.lineItemsRepo.createLineItemGroup(data);
+    }
+    async updateLineItemGroup(id:string,data:updateLineItemGroupDto){
+        return await this.lineItemsRepo.updateLineItemGroup(id,data);
+    }
+    async getGroupsByEstimationId(estimationId:string){
+        return await this.lineItemsRepo.getGroupsByEstimationId(estimationId);
+    }
+    async deleteLineItemGroup(id:string){
+        return await this.lineItemsRepo.deleteLineItemGroup(id);
+    }
+    async getAllGroups(){
+        return await this.lineItemsRepo.getAllGroups();
+    }
+    async getLineItemGroupById(id:string){
+        return await this.lineItemsRepo.getLineItemGroupById(id);
+    }
+//Line Items
+    async createLineItem(data:lineItemDto){
+        return await this.lineItemsRepo.createLineItem(data);
+    }
+    async updateLineItem(id:string,data:updateLineItemDto){
+        return await this.lineItemsRepo.updateLineItem(id,data);
+    }
+    async getLineItemsByGroupId(groupId:string){
+        return await this.lineItemsRepo.getLineItemsByGroupId(groupId);
+    }
+    async deleteLineItem(id:string){
+        return await this.lineItemsRepo.deleteLineItem(id);
+    }
+}
